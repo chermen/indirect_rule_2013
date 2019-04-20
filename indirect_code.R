d81b09a3fd0a705c3b6cc731ccd355e844ed38e8
Valery Dzutsati <vdzutsati@asu.edu>
2019-04-20 22:13:46 +0200

    no message

--- indirect_code.R
+++ indirect_code.R
@@ -1,10 +1,12 @@
-setwd("~")
+setwd("")
 library(foreign)
 library(coefplot)
 library(Zelig)
 library(nnet)
 library(MASS)
 library(xtable)
+library(lme4)
+library(lattice)
 
 #read in data
 ir1<-read.dta("ir1.dta")
@@ -79,47 +81,6 @@ ggsave(file="r.eps")
 b + geom_point() +
 scale_shape_manual(values=c(0,5,6))
 
-###alternative forest plot for black and white publications
-
-Alphas <- seq(1, 99, 2) / 100
-
-Multiplier <- qnorm(1 - Alphas / 2)
-zzTransparency <<- 1/(length(Multiplier)/4)
-CoefficientTables <- lapply(models, function(x){summary(x)$coef})
-TableRows <- unlist(lapply(CoefficientTables, nrow))
-
-if(modelnames[1] == ""){
-  ModelNameLabels <- rep(paste("Model", 1:length(TableRows)), TableRows)
-} else {
-  ModelNameLabels <- rep(modelnames, TableRows)
-}
-
-MatrixofModels <- cbind(do.call(rbind, CoefficientTables), ModelNameLabels)
-if(removeintercept == TRUE){
-  MatrixofModels <- MatrixofModels[!rownames(MatrixofModels) == "(Intercept)", ]
-}
-MatrixofModels <- data.frame(cbind(rownames(MatrixofModels), MatrixofModels))
-
-MatrixofModels <- data.frame(cbind(MatrixofModels, rep(Multiplier, each = nrow(MatrixofModels))))
-
-colnames(MatrixofModels) <- c("IV", "Estimate", "StandardError", "TValue", "PValue", "ModelName", "Scalar")
-MatrixofModels$IV <- factor(MatrixofModels$IV, levels = MatrixofModels$IV)
-MatrixofModels[, -c(1, 6)] <- apply(MatrixofModels[, -c(1, 6)], 2, function(x){as.numeric(as.character(x))})
-MatrixofModels$Emphasis <- by(1 - seq(0, 1, length = length(Multiplier) + 1)[-1], as.character(round(Multiplier, 5)), mean)[as.character(round(MatrixofModels$Scalar, 5))]
-
-OutputPlot <- qplot(data = MatrixofModels, x = IV, y = Estimate,
-                    ymin = Estimate - Scalar * StandardError, ymax = Estimate + Scalar * StandardError,
-                    ylab = NULL, xlab = NULL, alpha = I(zzTransparency), colour = I(gray(0)), geom = "blank")
-OutputPlot <- OutputPlot + geom_hline(yintercept = 0, lwd = I(7/12), colour = I(hsv(0/12, 7/12, 7/12)), alpha = I(5/12))
-OutputPlot <- OutputPlot + geom_linerange(data = MatrixofModels, aes(size = 1/Emphasis), alpha = I(zzTransparency), colour = I(gray(0)))
-OutputPlot <- OutputPlot + scale_size_continuous(legend = FALSE)
-OutputPlot <- OutputPlot + facet_grid(~ ModelName) + coord_flip() + geom_point(aes(x = IV, y = Estimate), colour = I(gray(0))) + theme_bw()
-return(OutputPlot)
-}
-
-models<-list(controls, response, full)
-
-SmoothCoefficientPlot(models, removeintercept=TRUE)
 
 ##Multinomial models
 ir1$rule3<-factor(ir1$rule)
@@ -162,23 +123,25 @@ summary(glm.0)
 
 #### controls
 
-glm.1<-zelig(rule2~noputin+norep+hardlife+trust+migrate+tag(1|reg), data=ir1, model="logit.mixed")
+glm.1<-glmer(rule2~noputin+norep+hardlife+trust+migrate+(1|reg), family=binomial, 
+             data=ir1)
 
 summary(glm.1)
 
 #### explanatory
 
-glm.2<-zelig(rule2~income+incomesq+ed+edsq+native+tension+
-               network+tag(1|reg), 
-             data=ir1, model="logit.mixed")
+glm.2<-glmer(rule2~income+incomesq+ed+edsq+native+tension+
+               network+(1|reg), family=binomial,
+             data=ir1)
              
 summary(glm.2)
 
 
 #### full
 
-glm.3<-zelig(rule2~noputin+norep+hardlife+trust+migrate+income+incomesq+ed+edsq+native+
-               tension+network+tag(1|reg), data=ir1, model="logit.mixed")
+glm.3<-glmer(rule2~noputin+norep+hardlife+trust+migrate+income+incomesq+ed+edsq+native+
+               tension+network+(1|reg), family=binomial,
+             data=ir1)
 
 summary(glm.3)
 
